c98c19beb27c1b98582cacd858bd51c115ceddab
Jamie Camilleri <jamescamilleri1196@gmail.com>
2022-05-20 16:31:52 +1000

    Added offset to Joystick

--- controllerSystem/main.py
+++ controllerSystem/main.py
@@ -1,7 +1,7 @@
 import serial
 
 import paho.mqtt.client as mqtt
-from tb_device_mqtt import TBDeviceMqttClient, TBPublishInfo
+from tb_device_mqtt import TBDeviceMqttClient
 
 ser = serial.Serial("/dev/ttyACM0", 9600)
 
@@ -12,11 +12,13 @@ moClient.connect("test.mosquitto.org")
 
 def main():
     while True:
-        
         data = parse_serial_input()
         data_arr = data.split(",")
         if len(data_arr) == 2:
-            mqtt_string = f"m {data_arr[0]} {data_arr[1]}"
+            data_arr[0] -= 30
+            if data_arr[0] < 0:
+                data_arr[0] = 0
+            mqtt_string = f"m {data_arr} {data_arr[1]}"
             moClient.publish("joystick/command", mqtt_string)
             mqtt_struct = {
                 "command" : mqtt_string 
