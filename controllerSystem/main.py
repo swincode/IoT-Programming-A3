4a4d940f443209edbb163ea6ad227a03c156433e
swincode <101907966@student.swin.edu.au>
2022-05-21 12:25:34 +1000

    add enabled state to controller

--- controllerSystem/main.py
+++ controllerSystem/main.py
@@ -5,44 +5,43 @@ import paho.mqtt.client as mqtt
 from tb_device_mqtt import TBDeviceMqttClient
 
 ser = serial.Serial("/dev/ttyACM0", 9600)
+disabled = False
+state = True
 
-tbClient = TBDeviceMqttClient("demo.thingsboard.io", "qSPi2bDBvBJaPJwcFrTX")
+tbClient = TBDeviceMqttClient("demo.thingsboard.io", "NmhyyW2DzT0Zb7C41PvS")
 moClient = mqtt.Client("joystick")
 tbClient.connect()
 moClient.connect("test.mosquitto.org")
+moClient.subscribe("joystick/disabled")
 
+def disabled_state():
+    disabled = not disabled
+    tbClient.send_attributes({"enabled" : disabled})
 
-import serial
-
-from tb_device_mqtt import TBDeviceMqttClient, TBPublishInfo
-
-ser = serial.Serial("/dev/ttyACM0", 9600)
-
-client = TBDeviceMqttClient("demo.thingsboard.io", "NmhyyW2DzT0Zb7C41PvS")
-# client = TBDeviceMqttClient("localhost", "token")
-client.connect()
-
+moClient.on_message = disabled_state
 
 def main():
-    state = True
     while True:
-        data = parse_serial_input()
-        if data == "toggle power":
-            state = not state
-            client.send_attributes({"power state": state})
-        data_arr = data.split(",")
-        if len(data_arr) == 2:
-            data_arr[0] = int(data_arr[0]) - 30
-            if data_arr[0] < 0:
-                data_arr[0] = 0
-            mqtt_string = f"m {data_arr[0]} {data_arr[1]}"
-            moClient.publish("joystick/command", mqtt_string)
-            mqtt_struct = {
-                "command" : mqtt_string 
-            }
-            tbClient.send_attributes(mqtt_struct)
+        if not disabled:
+            data = parse_serial_input()
+            if data == "toggle power":
+                state = not state
+                tbClient.send_attributes({"power state": state})
+            data_arr = data.split(",")
+            if len(data_arr) == 2:
+                data_arr[0] = int(data_arr[0]) - 30
+                if data_arr[0] < 0:
+                    data_arr[0] = 0
+                if data_arr[1] < 0:
+                    data_arr[1] = 0
+                mqtt_string = f"m {data_arr[0]} {data_arr[1]}"
+                moClient.publish("joystick/command", mqtt_string)
+                mqtt_struct = {
+                    "command" : mqtt_string 
+                }
+                tbClient.send_attributes(mqtt_struct)
 
-        print(data_arr)
+            print(data_arr)
 
 def parse_serial_input() -> str:
         """
