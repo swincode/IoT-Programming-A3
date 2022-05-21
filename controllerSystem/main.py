80120431f51b0eac72974e80988692b9744b7eb0
swincode <101907966@student.swin.edu.au>
2022-05-21 17:57:26 +1000

    fix a fix of a fix of a fix

--- controllerSystem/main.py
+++ controllerSystem/main.py
@@ -2,52 +2,34 @@
 import serial
 
 import paho.mqtt.client as mqtt
-from tb_device_mqtt import TBDeviceMqttClient
+
+from DataController import DataController
 
 class ControllerSystem:
     def __init__(self):
         self.ser = serial.Serial("/dev/ttyACM0", 9600)
         self.sendData = True
         self.powerState = True
-
-        self.tbClient = TBDeviceMqttClient("demo.thingsboard.io", "NmhyyW2DzT0Zb7C41PvS")
-        self.moClient = mqtt.Client("joystick")
-        self.tbClient.connect()
-        self.moClient.connect("test.mosquitto.org")
-        self.moClient.subscribe("joystick/state")
-
-        self.moClient.on_message = self.disabled_state
-
-    def disabled_state(self, client, userdata, message):
-        print("here")
-        stringRep = message.payload.decode("utf-8")
-        self.tbClient.send_attributes({"enabled" : stringRep})
-        if stringRep == "True":
-            self.sendData = True
-        else:
-            self.sendData = False
+        
+        self.data_controller = DataController("joystick")
+        self.data_controller.moClient.loop_start()
 
     def main(self):
         while True:
-            if self.sendData:
+            if self.data_controller.joystick_state:
                 data = self.parse_serial_input()
                 if data == "toggle power":
                     self.powerState = not self.powerState
-                    self.tbClient.send_attributes({"power state": self.powerState})
-                    self.moClient.publish("joystick/power", self.powerState)
+                    self.data_controller.send_attributes({"power state": self.powerState})
+                    self.data_controller.send_data("joystick/power", str(self.powerState))
                 data_arr = data.split(",")
                 if len(data_arr) == 2:
-                    data_arr[0] = int(data_arr[0]) - 30
-                    if int(data_arr[0]) < 0:
-                        data_arr[0] = 0
-                    if int(data_arr[1]) < 0:
-                        data_arr[1] = 0
                     mqtt_string = f"m {data_arr[0]} {data_arr[1]}"
-                    self.moClient.publish("joystick/command", mqtt_string)
+                    self.data_controller.send_data("joystick/command", mqtt_string)
                     mqtt_struct = {
                         "command" : mqtt_string 
                     }
-                    self.tbClient.send_attributes(mqtt_struct)
+                    self.data_controller.send_attributes(mqtt_struct)
 
                 print(data_arr)
 
